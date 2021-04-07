ac5a219666157ab9bda7941d8decfd0703ca55eb
Daniel Batista <dani_batista20@hotmail.com>
2021-04-07 14:43:28 +0100

    #2 Display a list

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './mockup-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +11,23 @@ export class HeroesComponent implements OnInit {
 
   title: string = "Heroes";
 
-  hero: Hero = {
-    id: 1,
-    name: "Super Man"
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: "Super Man"
+  // }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: any) {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
   }
 
 }
---
+++ src/app/components/heroes/mockup-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Super Man', universe: "Marvel" },
+  { id: 12, name: 'Iron Man', universe: "Marvel" },
+  { id: 13, name: 'Batman', universe: "DC" },
+  { id: 14, name: 'Spider Man', universe: "Marvel" },
+  { id: 15, name: 'Songoku', universe: "Dragon Ball" },
+  { id: 16, name: 'Vegeta', universe: "Dragon Ball" },
+];
