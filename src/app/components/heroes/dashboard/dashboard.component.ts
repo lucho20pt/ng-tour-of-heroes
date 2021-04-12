465af50ce5345da62402405f98f1e09b44d2ff1b
Daniel Batista <dani_batista20@hotmail.com>
2021-04-12 13:30:30 +0100

    #5 Add Navigation -> upd: dashboard getHeroes()

--- src/app/components/heroes/dashboard/dashboard.component.ts
+++ src/app/components/heroes/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,9 +11,21 @@ export class DashboardComponent implements OnInit {
 
   title: string = "Dashboard";
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(
+    public heroService : HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(
+        heroes => this.heroes = heroes.slice(1,4)
+      )
   }
 
 }
